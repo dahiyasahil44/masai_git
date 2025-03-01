f4ea88865f203de84ebbcf13e8eb8a59876d1a76
Sahil <imsahil440407@gmail.com>
2025-03-02 02:21:18 +0530

    Dynamic User Role Message with Object Properties - Solved

--- dynamic_user_role/index.js
+++ dynamic_user_role/index.js
@@ -20,3 +20,11 @@
 
 // Output: "Admin Access Revoked"
 
+
+let user = { name: "Alice", role: "admin", active: false };
+
+let output = (user.role=='admin')?
+                ((user.active)?"Admin Access Granted!":"Admin Access Revoked")
+            :((user.role=='user')?((user.active)?"User Access Granted!":"User Access Revoked"):"Access Denied");
+
+console.log(output);
