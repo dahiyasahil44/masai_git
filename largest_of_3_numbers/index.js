472ea621800b04dec1b0032c47efef8852e69870
Sahil <imsahil440407@gmail.com>
2025-03-02 01:53:19 +0530

    Largest of Three Numbers Using Ternary Operator - Problem Solved

--- largest_of_3_numbers/index.js
+++ largest_of_3_numbers/index.js
@@ -3,3 +3,9 @@
 
 // Example: let num1 = 45, num2 = 78, num3 = 33;
 
+let num1 = 45, num2 = 78, num3 = 133;
+
+let result = (num1>num2)?((num1>num3)?num1:num3)
+            :((num2>num3)?num2:num3);
+
+console.log(result);
